c680032a8e43cc1c21e5d856d846f664302cfb82
rufus <rufusfreedom8@gmail.com>
2025-01-27 11:45:33 +0200

    Персонаж подбрасывает фрукт на верхний этаж

--- Assets/Scripts/Items/ObjectInstantiater.cs
+++ Assets/Scripts/Items/ObjectInstantiater.cs
@@ -6,17 +6,30 @@ public class ObjectInstantiater : MonoBehaviour
 {
     [SerializeField] private GameObject objectToSpawn; 
     [SerializeField] private Transform thrownPoint;
-    public event Action OnFruitInstantiated;
+    //public event Action OnFruitInstantiated;
 
 
     public void InstantiateObject()
     {
         if (objectToSpawn != null && thrownPoint != null)
         {
+            // Инстанцируем объект
             GameObject newObject = Instantiate(objectToSpawn, thrownPoint.position, thrownPoint.rotation);
 
-            PushRigidBody pushRigidBody = newObject.GetComponent<PushRigidBody>(); 
-            OnFruitInstantiated?.Invoke();
+            // Найдем компонент PushRigidBody и передадим ему ссылку на себя (ObjectInstantiater)
+            PushRigidBody pushRigidBody = newObject.GetComponent<PushRigidBody>();
+            if (pushRigidBody != null)
+            {
+                pushRigidBody.objectInstantiater = this; // Назначаем ссылку
+                pushRigidBody.Push(); // Подкидываем объект
+            }
+
+            // Аналогично с другим компонентом
+            ChangerLayer changerLayer = newObject.GetComponent<ChangerLayer>();
+            if (changerLayer != null)
+            {
+                changerLayer.ChangeTheLayer(); // Меняем слой
+            }
         }
     }
 }
