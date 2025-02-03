8caf71063fc16c1cff3c538e912f53484dd5abed
rufus <rufusfreedom8@gmail.com>
2025-02-03 13:45:19 +0200

    Создана анимация бота. (бег, покой).

--- Assets/Scripts/Bot/BotMovement.cs
+++ Assets/Scripts/Bot/BotMovement.cs
@@ -21,15 +21,21 @@ public class BotMovement : MonoBehaviour, IUpdatable
     [SerializeField] private Transform spawnPoint;
 
     private Vector3 targetPosition;
+    private Animator animator;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+       
     }
+
     public IEnumerator RandomMovementRoutine()
     {
+        
         while (true)
         {
+            
             moveDirection = Random.value > 0.5f ? 1f : -1f;
             float moveTime = Random.Range(minMoveTime, maxMoveTime);
             isMoving = true;
@@ -63,7 +69,7 @@ public class BotMovement : MonoBehaviour, IUpdatable
     private void FlipCharacter()
     {
         Vector3 scale = transform.localScale;
-        scale.x = Mathf.Abs(scale.x) * -moveDirection;
+        scale.x = Mathf.Abs(scale.x) * moveDirection;
         transform.localScale = scale;
     }
 
@@ -76,33 +82,40 @@ public class BotMovement : MonoBehaviour, IUpdatable
             {
                 Vector2 direction = (targetPosition - transform.position).normalized;
                 rb.linearVelocity = new Vector2(direction.x * speed, rb.linearVelocity.y);
+                moveDirection = Mathf.Sign(direction.x);
+                animator.SetFloat("xVelocity", Mathf.Abs(direction.x));
             }
             else
             {
                 // Останавливаемся, когда достигли цели
                 rb.linearVelocity = Vector2.zero;
                 isMovingToTarget = false; // Сбрасываем флаг
+                animator.SetFloat("xVelocity", 0);
             }
         }
         else if (isMoving)
         {
             // Обычное случайное движение
             rb.linearVelocityX = moveDirection * speed;
+            animator.SetFloat("xVelocity", Mathf.Abs(moveDirection));
         }
         else
         {
             // Останавливаем движение
             rb.linearVelocity = Vector2.zero;
+            animator.SetFloat("xVelocity", 0);
         }
         ChangeMoveDirection();
         FlipCharacter();
     }
     public IEnumerator DropStonesRoutine()
     {
+        yield return new WaitForSeconds(2f);
+
         for (int i = 0; i < 3; i++)
         {
             Instantiate(stonePrefab, spawnPoint.position, Quaternion.identity);
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(1.5f);
         }
     }
 
@@ -116,124 +129,3 @@ public class BotMovement : MonoBehaviour, IUpdatable
         UpdateManager.Instance.Unregister(this); // Отменяем регистрацию при отключении
     }
 }
-
-
-
-
-
-
-//public class BotMovement : MonoBehaviour, IUpdatable
-//{
-//    [SerializeField] private float speed = 2f;
-//    [SerializeField] private float minMoveTime = 2f;
-//    [SerializeField] private float maxMoveTime = 5f;
-//    [SerializeField] private float minStopTime = 1f;
-//    [SerializeField] private float maxStopTime = 3f;
-
-//    private float moveDirection;
-//    private Rigidbody2D rb;
-//    private bool isMoving = true;
-
-//    [SerializeField] private GameObject stonePrefab;
-//    [SerializeField] private Transform spawnPoint;
-
-//    private Vector3 targetPosition;
-//    private Coroutine randomMovementCoroutine;
-
-//    private void Start()
-//    {
-//        rb = GetComponent<Rigidbody2D>();
-//        //randomMovementCoroutine = StartCoroutine(RandomMovementRoutine());
-//    }
-
-//    public IEnumerator RandomMovementRoutine()
-//    {
-//        while (true)
-//        {
-//            moveDirection = Random.value > 0.5f ? 1f : -1f;
-//            float moveTime = Random.Range(minMoveTime, maxMoveTime);
-//            isMoving = true;
-//            rb.linearVelocityX = moveDirection * speed;
-//            yield return new WaitForSeconds(moveTime);
-
-//            isMoving = false;
-//            float stopTime = Random.Range(minStopTime, maxStopTime);
-//            yield return new WaitForSeconds(stopTime);
-//        }
-//    }
-
-//    public IEnumerator MoveToTarget(Vector3 position)
-//    {
-//        targetPosition = position;
-//        while (Vector2.Distance(transform.position, targetPosition) > 0.1f)
-//        {
-//            Vector2 direction = (targetPosition - transform.position).normalized;
-//            rb.linearVelocityX = direction.x * speed;
-//            yield return null;
-//        }
-
-
-//    }
-
-//    public IEnumerator DropStonesRoutine()
-//    {
-//        for (int i = 0; i < 3; i++)
-//        {
-//            Instantiate(stonePrefab, spawnPoint.position, Quaternion.identity);
-//            yield return new WaitForSeconds(2f);
-//        }
-//    }
-
-//    public void SetIsMoving(bool value)
-//    {
-//        isMoving = value;
-//    }
-
-//    public void SetMoveDirection(float direction)
-//    {
-//        moveDirection = direction;
-//    }
-
-//    private void ChangeMoveDirection()
-//    {
-//        if (transform.position.x <= -8.4 && moveDirection < 0)
-//        {
-//            moveDirection = 1;
-//        }
-//        else if (transform.position.x >= 8.2 && moveDirection > 0)
-//        {
-//            moveDirection = -1;
-//        }
-//    }
-
-//    private void FlipCharacter()
-//    {
-//        Vector3 scale = transform.localScale;
-//        scale.x = Mathf.Abs(scale.x) * -moveDirection;
-//        transform.localScale = scale;
-//    }
-
-//    public void CustomUpdate()
-//    {
-//        if (isMoving)
-//        {
-//            rb.linearVelocityX = moveDirection * speed;
-//        }
-//        else
-//        {
-//            rb.linearVelocityX = 0f;
-//        }
-
-//        ChangeMoveDirection();
-//        FlipCharacter();
-//    }
-//private void OnEnable()
-//{
-//    UpdateManager.Instance.Register(this); // Регистрируем объект для обновления
-//}
-
-//private void OnDisable()
-//{
-//    UpdateManager.Instance.Unregister(this); // Отменяем регистрацию при отключении
-//}
-//}
