f95c1c3410bb88caa7d67ef1af89986f1b43f452
rufus <rufusfreedom8@gmail.com>
2025-02-21 10:42:02 +0200

    Убирается подсветка объекта (на канвасе) если его нет в наличии.

--- Assets/Scripts/UnderTheHood/InventoryPlayer.cs
+++ Assets/Scripts/UnderTheHood/InventoryPlayer.cs
@@ -1,23 +1,17 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class InventoryPlayer : MonoBehaviour
 {
     [SerializeField] private int currentDynamites;
 
+    public UnityEvent NumberOfBombsZero;
+
     public int CurrentDynamites
     {
         get => currentDynamites;
-
-        //set
-        //{
-        //    if (value >= 0 && value <= 3)
-        //    {
-        //        currentDynamites = value;
-        //        UpdateDynamiteCountText();
-        //    }
-        //}
     }
 
     [SerializeField] private TMP_Text dynamiteCountText;
@@ -35,9 +29,20 @@ public class InventoryPlayer : MonoBehaviour
         {
             currentDynamites--;
             UpdateDynamiteCountText();
+            CheckNumberOfBombs();
         }
 
     }
+
+    public void CheckNumberOfBombs()
+    {
+        if (currentDynamites == 0)
+        { 
+            NumberOfBombsZero?.Invoke();
+            Debug.Log("событие что бомбы закончились сработало");
+        }
+    }
+
     private void UpdateDynamiteCountText()
     {
 
