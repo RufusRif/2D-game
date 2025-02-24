ce4f8ae2167a94660d3d8b8794d7b3ce140b8dfc
rufus <rufusfreedom8@gmail.com>
2025-02-24 12:50:14 +0200

    Замена метода Update в HealthSystem на UpdateLivesUI, добавлены методы TakeDamage, Heal

---
+++ Assets/Scripts/UnderTheHood/HealthSystem.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class HealthSystem : MonoBehaviour
+{
+
+    [SerializeField] private Image[] lives;
+    [SerializeField] private Sprite fullLive;
+    [SerializeField] private Sprite emptyLive;
+    [SerializeField] private int health;
+    [SerializeField] private int numberOfLives;
+
+    private void Start()
+    {
+        
+        UpdateLivesUI();
+    }
+
+    private void UpdateLivesUI()
+    {
+        for (int i = 0; i < lives.Length; i++)
+        {
+            if (i < health)
+            {
+                lives[i].sprite = fullLive;
+            }
+            else
+            {
+                lives[i].sprite = emptyLive;
+            }
+
+            lives[i].enabled = i < numberOfLives;
+
+
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        health = Mathf.Clamp(health, 0, numberOfLives);
+        UpdateLivesUI();
+    }
+
+    public void Heal(int amount)
+    {
+        health += amount;
+        health = Mathf.Clamp(health, 0, numberOfLives);
+        UpdateLivesUI();
+    }
+}
