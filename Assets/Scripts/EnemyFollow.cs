ca20e745f154a52391499a7357577e73d489ab21
rufus <rufusfreedom8@gmail.com>
2025-03-10 00:19:48 +0200

    Метод Update в EnemyFolow заменен на CustemUpdate

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
 
-public class EnemyFollow : MonoBehaviour
+public class EnemyFollow : MonoBehaviour, IUpdatable
 {
     [SerializeField] float speed;
     [SerializeField] Transform player;
-    void Update()
+   public void CustomUpdate()
     {
         transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+    private void OnEnable()
+    {
+        UpdateManager.Instance.Register(this);
+    }
+
+    private void OnDisable()
+    {
+        UpdateManager.Instance.Unregister(this);
     }
 }
